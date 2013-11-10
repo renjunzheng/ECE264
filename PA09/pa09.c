e58f6793d5d550bef9e3dbd38fa9934d00c5a6bf
Renjun Zheng <renjun@renjun-Dell-System-XPS-L502X.(none)>
2013-11-10 18:07:48 -0500

    finish some part, still need to do main, stack, print

--- PA09/pa09.c
+++ PA09/pa09.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pa09.h"
 
+int main ( int argc , char ** argv )
+{
+  int i; //index
+
+  //Check argc, print error message if the number of arguments are not correct
+  if (argc != 3)
+    {
+      printf("usage: ./pa09 <input file> <output file>\n");
+      return EXIT_FAILURE;
+    }
+
+  //Open the input file, check if the file has been opened correctly, if not, print error message
+  FILE * fptr = NULL;
+  fptr = fopen(argv[1], "r");
+  if (fptr == NULL)
+    {
+      printf("File error!\n");
+      return EXIT_FAILURE;
+    }
+  
+  //Call a function to create Huffman Tree, return the head of the tree
+  HuffNode * array = NULL;
+  array = HuffNode_create(value);// what is this value?
+
+  //Open an output file, use your own modified function postOrderPrint to print the traverse of the tree
+  fptr = fopen(argv[2], "w");
+  HuffNode_print(fptr, array);
+  if (fptr == NULL)
+    {
+      printf("output file error\n");
+      return EXIT_FAILURE;
+    }
+
+  HuffNode_destroy(array);
+  fclose(fptr);
+}
--- PA09/utility.c
+++ PA09/utility.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include "pa09.h"
+
+/*Write a function that can create Huffman Tree based on input
+(You will need to check if the input is char-based or bit-based,
+they are different, so your create Huffman Tree should be able
+to handle both cases)
+*/
+HuffNode * HuffNode_create(int value)
+{
+
+}
+
+
+
+
+//Write functions to perform push and pop to maintain the Stack
